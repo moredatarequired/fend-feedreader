7f070f347099bef1c08849b522f878e1b57a29d4
Hugh <hugh.wimberly@gmail.com>
2018-01-08 11:33:49 -0800

    Test for non-empty names and URLs for each feed.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,16 +27,26 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
+        /* Check that each feed in the allFeeds object has a URL 
+         * defined and that the URL is not empty.
          */
-
+        it ('have non-empty URLs', function() {
+            for (let feed of allFeeds) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url).not.toBe('');
+            }
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it ('have non-empty names', function() {
+            for (let feed of allFeeds) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe('');
+            }
+        });
     });
 
 
